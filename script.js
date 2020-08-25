fe042d5f8579b36903064c5aff317ca220c770da
nedretb <nedret.jr@gmail.com>
2020-08-25 10:30:06 +0200

    finished

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 let grid = document.getElementById("container");
 let gridSize = document.getElementById("grid-size");
 let clear = document.getElementById("clear");
-let changeColors = document.getElementById("change-colors");
-let colorSelector = 0;
+let blackColor = document.getElementById("black");
+let sprayColor = document.getElementById("spray");
+let randomColor = document.getElementById("random");
+let mode = "basic";
 
 gridSize.addEventListener('change', clearBox);
 clear.addEventListener('click', clearBox);
-//changeColors.addEventListener('click', colorSelector());
+blackColor.addEventListener('click', () =>{
+    mode = "basic";
+});
+sprayColor.addEventListener('click', () => {
+    mode = "spray";
+});
+randomColor.addEventListener('click', () =>{
+    mode = "random";
+})
 
 function createGrid(size){
     grid.style.setProperty('--grid-rows', size);
@@ -22,9 +32,32 @@ function createGrid(size){
 function colorBox(){
     let box = document.querySelectorAll('.grid-box');
     box.forEach(box => box.addEventListener('mouseover', (event) =>{
-        event.target.style.backgroundColor = "black";
+        event.target.style.backgroundColor = chooseColor();
+        function chooseColor(){
+            let color = ""
+            if (mode == "basic"){
+                color = "black";
+            }
+            else if (mode == "spray"){
+                let shade = event.target.style.backgroundColor;
+                if(shade==""){
+                    shade = "rgba(0, 0, 0, 0)"
+                }
+                let current = shade.split(', ');
+                current[3] = parseFloat(current[3]);
+                current[3] += 0.2;
+                color = current.join(', ');
+                color = color + ")";
+            }
+            else if (mode == "random"){
+                color = "rgb( " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) +")";
+            }
+            return color;
+        }
+        
     }))
 }
+
 
 function clearBox(){
     let boxes = document.querySelectorAll('.grid-box');
